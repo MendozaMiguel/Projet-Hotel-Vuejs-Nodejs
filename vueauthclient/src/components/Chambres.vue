e49b896f907c1b466c6c09c9884d1f849e536729
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-02-15 17:00:27 +0100

    affichage des chambres via l'api

---
+++ vueauthclient/src/components/Chambres.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-for="chambre in chambres">
+      <div>{{chambre.id}}</div>
+      <div>{{chambre.name}}</div>
+      <div>{{chambre.description}}</div>
+      <div>{{chambre.price}}</div>
+      <div>{{chambre.image}}</div>
+      <img :src="`../../static/chambres/img_${chambre.image}`">
+      <div>{{chambre.category}}</div>
+      <br><br>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import router from '../router'
+import axios from 'axios'
+
+export default {
+  name:'Chambres',
+  data(){
+    return{
+      chambres:{
+        id:1,
+        name:'typeChambre',
+        descritpion:'Description de la chambre',
+        price: 99,
+        image: 'nom_image',
+        category: 'categorie_chambre'}
+    }
+  },
+  methods:{
+    getData: function(){
+      let self = this;
+      axios.get('api/chambres')
+      .then((response)=>{
+        console.log(response.data.chambres);
+        self.$set(this,"chambres",response.data.chambres)
+      })
+      .catch(errors=>console.log(errors))
+    }
+  },
+  mounted(){
+   this.getData()
+  }
+}
+</script>

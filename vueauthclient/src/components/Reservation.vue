e6f88a081451703f2394d4084040bc18adf7dbc0
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-02-15 20:20:30 +0100

    creation composant Reservation+initialisation datepicker

---
+++ vueauthclient/src/components/Reservation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Période de réservation (*)</h1>
+    <datepicker :language="fr" v-model="vModelExample"></datepicker>
+    <p>{{ vModelExample }}</p>
+  </div>
+</template>
+
+<script>
+import Datepicker from 'vuejs-datepicker';
+import {fr} from 'vuejs-datepicker/dist/locale'
+
+export default {
+  name:'Reservation',
+  data(){
+    return{
+      fr:fr,
+      vModelExample: null,
+    }
+  },
+  components: {
+     'Datepicker' : Datepicker
+  },
+  mounted(){
+    console.log(Datepicker)
+  }
+}
+</script>

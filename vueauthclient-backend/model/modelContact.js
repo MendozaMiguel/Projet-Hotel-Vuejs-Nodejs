3112f7ed8c03ecf786dfa86443137745074e6878
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-02-15 16:55:41 +0100

    requete sql pour contact

--- vueauthclient-backend/model/modelContact.js
+++ vueauthclient-backend/model/modelContact.js
@@ -1,6 +1,7 @@
 const con = require("../configuration/mySqlConfig");
 
 let myreq="Select * from contact";
+
 function getReqContact(){
   return con.then(function(con){
     let resultat = con.query(myreq);

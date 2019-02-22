666c949b105e634d2186b2bb48a05ee386ccdbc9
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-02-22 12:47:15 +0100

    update model contact

--- vueauthclient-backend/model/modelContact.js
+++ vueauthclient-backend/model/modelContact.js
@@ -3,8 +3,8 @@ const con = require("../configuration/mySqlConfig");
 let myreq="Select * from contact";
 
 function getReqContact(){
-  return con.then(function(con){
-    let resultat = con.query(myreq);
+  return con.then(function(conn){
+    let resultat = conn.query(myreq);
     return resultat
   })
 }

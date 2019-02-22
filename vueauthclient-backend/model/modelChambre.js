a6af0a1d2688b39b3b531993ef960715a3b8499e
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-02-22 12:46:41 +0100

    update model chambre

--- vueauthclient-backend/model/modelChambre.js
+++ vueauthclient-backend/model/modelChambre.js
@@ -3,8 +3,8 @@ const con = require("../configuration/mySqlConfig");
 let myreq="Select * from chambre";
 
 function getReqChambres(){
-  return con.then(function(con){
-    let chambres = con.query(myreq);
+  return con.then(function(conn){
+    let chambres = conn.query(myreq);
     return chambres
   })
 }

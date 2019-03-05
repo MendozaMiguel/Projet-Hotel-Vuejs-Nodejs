368a59be7ec012e515a5a057df6807ef7ebfbecb
MendozaMiguel <samirmiguelvoillaume@gmail.com>
2019-03-05 17:31:00 +0100

    decomment login secure

--- server/controller/utilisateurController.js
+++ server/controller/utilisateurController.js
@@ -3,70 +3,70 @@ const query = require('./../model/utilisateur');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-function utilisateurGet (req, res) {
-  // if (!req.session.admin === true) {
-  //     res.send('ERROR404')
-  // }
-  query.getAll().then(function(resultat, err){
-      if (err) throw err;
-      res.send({resultat: resultat});
+function utilisateurGet(req, res) {
+  if (!req.session.admin === true) {
+      res.redirect('/login')
+  }
+  query.getAll().then(function (resultat, err) {
+    if (err) throw err;
+    res.send({ resultat: resultat });
   })
 }
 
-function getEnregistrement(req, res){
+function getEnregistrement(req, res) {
   if (!req.session.admin === true){
       res.send('ERROR404');
   }
-    res.render('pageFormulaire');
+  res.send('/api/enregistrement');
 }
 
-function postEnregistrement(req, res){
+function postEnregistrement(req, res) {
+  let prenom = req.body.prenom;
+  let motdepasse = req.body.motDePass;
   if (prenom === null || motdepasse === null) {
-      res.render('pageFormulaire');
+    res.send('/api/enregistrement');
   } else {
-      // let prenom = req.body.prenom;
-      // let motdepasse = req.body.motDePass;
-      let hash = bcrypt.hashSync(motdepasse, saltRounds);
-      //On prepare l'insertion avec les '?,?' que l'on remplace juste après avec [prenom,motdepasse]
-      query.postEnregistrement(prenom, hash).then(function (resultat, err) {
-          if (err) throw err;
-          res.redirect('/api/utilisateurs');
-      });
+    let hash = bcrypt.hashSync(motdepasse, saltRounds);
+    //On prepare l'insertion avec les '?,?' que l'on remplace juste après avec [prenom,motdepasse]
+    query.postEnregistrement(prenom, hash).then(function (resultat, err) {
+      if (err) throw err;
+      res.send('/api/utilisateurs');
+    });
   }
 }
 
-function deleteUser(req, res){
+function deleteUser(req, res) {
   var idUtlisateurs = req.params.idUtilisateurs;
   query.deleteUser(idUtlisateurs).then(function (resultat, err) {
-      if (err) throw err;
-      res.redirect('/api/utilisateurs');
+    if (err) throw err;
+    res.send('/api/utilisateurs');
   });
 }
 
 
-function modifyUserGet(req, res){
+function modifyUserGet(req, res) {
   var idUtlisateurs = req.params.idUtilisateurs;
   query.modifyUserGet(idUtlisateurs).then(function (resultat, err) {
-      if (err) throw err;
-      res.render('pageModifier', {resultat: resultat});
+    if (err) throw err;
+    res.render('pageModifier', { resultat: resultat });
   });
 }
 
-function modifyUserPost(req, res){
+function modifyUserPost(req, res) {
   var motDeDasseUtilisateur = req.body.motDePass;
   let hash = bcrypt.hashSync(motDeDasseUtilisateur, saltRounds);
-  query.modifyUserPost(req,hash).then(function (resultat,err,meta) {
-      if (err) throw err;
-      res.redirect('/api/utilisateurs');
+  query.modifyUserPost(req, hash).then(function (resultat, err, meta) {
+    if (err) throw err;
+    res.send('/api/utilisateurs');
   });
 }
 
 
 module.exports = {
-    utilisateurGet:utilisateurGet,
-    getEnregistrement:getEnregistrement,
-    postEnregistrement:postEnregistrement,
-    deleteUser:deleteUser,
-    modifyUserGet:modifyUserGet,
-    modifyUserPost:modifyUserPost,
+  utilisateurGet: utilisateurGet,
+  getEnregistrement: getEnregistrement,
+  postEnregistrement: postEnregistrement,
+  deleteUser: deleteUser,
+  modifyUserGet: modifyUserGet,
+  modifyUserPost: modifyUserPost,
 };
